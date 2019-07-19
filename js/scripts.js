1e5e78ab92daf2b48304ae8341aadb64c34d6a17
WendyOmollo <omollow280@gmail.com>
2019-07-19 18:25:15 +0300

    Add opacity to the first three pictures in the portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,72 @@
 $(document).ready(function(){
     $("#designer").click(function(){
         $("#designer").hide();
-        $("#designing").show();
+        $("#designing").fadeIn();
     });
 });
 $(document).ready(function(){
     $("#designing").click(function(){
         $("#designing").hide();
-        $("#designer").show();
+        $("#designer").fadeIn();
     });
 });
 $(document).ready(function(){
     $("#developer").click(function(){
         $("#developer").hide();
-        $("#developing").show();
+        $("#developing").fadeIn();
     });
 });
 $(document).ready(function(){
     $("#developing").click(function(){
         $("#developing").hide();
-        $("#developer").show();
+        $("#developer").fadeIn();
     });
 });
 $(document).ready(function(){
     $("#producer").click(function(){
         $("#producer").hide();
-        $("#production").show();
+        $("#production").fadeIn();
     });
 });
 $(document).ready(function(){
     $("#production").click(function(){
         $("#production").hide();
-        $("#producer").show();
+        $("#producer").fadeIn();
     });
 });
+$(document).ready(function(){
+    $("#onework").mouseenter(function(){
+    $("#workone").show();
+    }).mouseleave(function(){
+        $("#workone").hide();
+    });
+    $("#twowork").mouseenter(function(){
+    $("#worktwo").show();
+    }).mouseleave(function(){
+        $("#worktwo").hide();
+        });
+    $("#threework").mouseenter(function(){
+    $("#workthree").show();
+    }).mouseleave(function(){
+        $("#workthree").hide();
+            });
+
+
+ });
+// $(document).ready(function(){
+// $("form#entry").submit(function(event){
+
+//     var informations = ["name","email","output"];
+//     informations.forEach(function(information){
+//         var getInfo = $("input#" + information).val();
+//     }
+//         if(getInfo!==""){
+//         alert("Please enter your name");
+//     }
+//     else{
+//         alert("We have received your message.Thank you" + name + email );
+//         }
+
+//     event.preventDefault();
+//     });
+// });
